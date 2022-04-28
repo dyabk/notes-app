33c7bf9f467813674a71ce58dc73568b9992aaa3
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 16:21:13 -0400

    Set a longer timeout for Jest. Wrote two more tests using the asynchronous function and expect method instead of using callback functions to access the data in promises

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -9,6 +9,18 @@ test("notes are returned as json", async () => {
     .get("api/notes")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+}, 100000);
+
+test("there are two notes", async () => {
+  const response = await api.get("api/notes");
+
+  expect(response.body).toHaveLength(2);
+});
+
+test("the first note is about HTTP methods", async () => {
+  const response = await api.get("api/notes");
+
+  expect(response.body[0].content).toBe("HTML is easy");
 });
 
 afterAll(() => {
