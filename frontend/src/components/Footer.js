1e695c013a0fb567d346c13b3834d24cb6acc24f
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 00:19:09 -0400

    Created

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    const footerStyle = {
-        color: 'green',
-        fontStyle: 'italic',
-        fontSize: 16
-    }
-    return (
-        <div style={footerStyle}>
-            <br />
-            <em>Note app, Department of Computer Science, Florida International University, 2022</em>
-        </div>
-    )
-}
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, Florida International
+        University, 2022
+      </em>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
