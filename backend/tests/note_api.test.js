8a4f0bbe49b1cc3454fd4f3a4e4ab9a91386a286
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 21:35:27 -0400

    Fixed the important value of the initialNotes list. Modified the API tests

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -13,7 +13,7 @@ const initialNotes = [
   {
     content: "Browser can execute only JavaScript",
     date: new Date(),
-    important: True,
+    important: true,
   },
 ];
 
@@ -32,16 +32,17 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 }, 100000);
 
-test("there are two notes", async () => {
+test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
 
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
-test("the first note is about HTTP methods", async () => {
+test("a specific note is within the returned notes", async () => {
   const response = await api.get("/api/notes");
 
-  expect(response.body[0].content).toBe("HTML is easy");
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only JavaScript");
 });
 
 afterAll(() => {
