b307d627a57bd195c3fec43ae6c4b6b55612f42e
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 23:17:05 -0400

    Created an addutional test for the POST API request before refactoring the method in the router

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -45,6 +45,26 @@ test("a specific note is within the returned notes", async () => {
   expect(contents).toContain("Browser can execute only JavaScript");
 });
 
+test("a valid note can be added", async () => {
+  const newNote = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
+
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+
+  expect(response.body).toHaveLength(intitialNotes.length + 1);
+  expect(contents).toContain("async/await simplifies making async calls");
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
