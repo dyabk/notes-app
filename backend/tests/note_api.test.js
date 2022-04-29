5407d9269cfc6fdf7b7c66832464cd7362db6415
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 21:19:43 -0400

    Set the database to initialize before every test with the beforeEach function

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,24 +1,45 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    date: new Date(),
+    important: True,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
-    .get("api/notes")
+    .get("/api/notes")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 }, 100000);
 
 test("there are two notes", async () => {
-  const response = await api.get("api/notes");
+  const response = await api.get("/api/notes");
 
   expect(response.body).toHaveLength(2);
 });
 
 test("the first note is about HTTP methods", async () => {
-  const response = await api.get("api/notes");
+  const response = await api.get("/api/notes");
 
   expect(response.body[0].content).toBe("HTML is easy");
 });
