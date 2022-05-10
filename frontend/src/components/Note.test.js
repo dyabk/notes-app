8bca56115730fa7ce9c209702e199bd9cc01ddf8
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-10 15:35:12 -0400

    Wrote the test to verify clicking the button calls the function only once

--- frontend/src/components/Note.test.js
+++ frontend/src/components/Note.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Note from "./Note";
 
 test("renders content", () => {
@@ -11,19 +12,41 @@ test("renders content", () => {
 
   render(<Note note={note} />);
 
-  /*
+  //screen.debug();
+
   const element = screen.getByText(
     "Component testing is done with react-testing-library"
   );
-  */
+  //screen.debug(element);
 
+  //the expect is not needed; the test failes if getByText fails
+  expect(element).toBeDefined();
+
+  //use CSS-selectors to find rendered elements by using the method querySelector
+  //of the object container that is one of the fields returned by the render
+  /*
   const { container } = render(<Note note={note} />);
 
   const div = container.querySelector(".note");
   expect(div).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
+  */
+});
 
-  //the expect is not needed; the tst failes if getByText fails
-  //expect(element).toBeDefined();
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = jest.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("make not important");
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
