bb98d0b2a69de9532d4942ee4a9b829b41c3d027
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-11 09:55:04 -0400

    Added a test for <NoteForm />. Cleared the act() console warning

--- frontend/src/components/Togglable.test.js
+++ frontend/src/components/Togglable.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Togglable from "./Togglable";
 
@@ -25,21 +25,19 @@ describe("<Togglable />", () => {
   });
 
   test("after clicking the button, children are displayed", async () => {
-    const user = userEvent.setup();
     const button = screen.getByText("show...");
-    await user.click(button);
+    fireEvent.click(button);
 
     const div = container.querySelector(".togglableContent");
     expect(div).not.toHaveStyle("display: none");
   });
 
   test("toggled content can be closed", async () => {
-    const user = userEvent.setup();
     const button = screen.getByText("show...");
-    await user.click(button);
+    fireEvent.click(button);
 
     const closeButton = screen.getByText("cancel");
-    await user.click(closeButton);
+    fireEvent.click(closeButton);
 
     const div = container.querySelector(".togglableContent");
     expect(div).toHaveStyle("display: none");
