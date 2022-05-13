6a5f834907b99c66546de253a000f2c3ed6e6bad
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-12 23:34:20 -0400

    Added a Cypress test

--- frontend/cypress/integration/note_app.spec.js
+++ frontend/cypress/integration/note_app.spec.js
@@ -68,6 +68,20 @@ describe("Note app", function () {
         cy.contains("another note cypress").contains("make important").click();
         cy.contains("another note cypress").contains("make not important");
       });
+
+      describe("and several notes exist", function () {
+        beforeEach(function () {
+          cy.createNote({ content: "first note", important: false });
+          cy.createNote({ content: "second note", important: false });
+          cy.createNote({ content: "third note", important: false });
+        });
+
+        it("one of those can be made important", function () {
+          cy.contains("second note").parent().find("button").as("theButton");
+          cy.get("@theButton").click();
+          cy.get("@theButton").should("contain", "make not important");
+        });
+      });
     });
   });
 });
